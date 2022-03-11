51e1894ff4fef6897d47b67e416eb58315c997b2
saheenshoukath <saheenshoukath@gmail.com>
2022-03-12 00:59:29 +0530

    Added prettier config

---
+++ packages/config/prettier-preset.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  arrowParens: "always",
+  importOrder: ["^@lib/(.*)$", "^@components/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+}
